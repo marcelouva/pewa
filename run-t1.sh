a457ab058276854ed70e5b7ab006debcc236d748
MUva <uva@dc.exa.unrc.edu.ar>
2017-04-11 13:54:31 -0300

    comit inicial

---
+++ run-t1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd bin 
+python pewa.py  stack stack_contains 1;
+python pewa.py  stack stack_empty 1;
+python pewa.py  stack stack_firstElement 1;
+python pewa.py  stack stack_peek 1;
+python pewa.py  stack stack_pop 1;
+python pewa.py  stack stack_push 1;
+python pewa.py  stack stack_search 1;
+python pewa.py  stack vector_add 1;
+python pewa.py  stack vector_addElement 1;
+python pewa.py  stack vector_addIndexItem 1;
+python pewa.py  stack vector_clear 1;
+python pewa.py  stack vector_elementAt 1;
+python pewa.py  stack vector_get 1;
+python pewa.py  stack vector_get_first 1;
+python pewa.py  stack vector_insertElementAt 1;
+python pewa.py  stack vector_lastElement 1;
+python pewa.py  stack vector_remove 1;
+python pewa.py  stack vector_removeAllElements 1;
+python pewa.py  stack vector_removeElement 1;
+python pewa.py  stack vector_removeElementAt 1;
+python pewa.py  stack vector_removeIndex 1;
+python pewa.py  stack vector_set 1;
+python pewa.py  stack vector_setElement 1;
+python pewa.py  stack vector_setSize 1;
+python pewa.py  stack vector_size 1;
